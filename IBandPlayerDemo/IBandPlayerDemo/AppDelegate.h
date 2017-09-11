eb48e39468f2df938538d28b6ee7fc5c8282176f
yogevbar <yogevbarber@gmail.com>
2017-09-11 18:12:20 +0300

    first commit

---
+++ IBandPlayerDemo/IBandPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBandPlayerDemo
+//
+//  Created by Yogev Barber on 11/09/2017.
+//  Copyright © 2017 IBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IBandPlayerDemo/IBandPlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IBandPlayerDemo
+//
+//  Created by Yogev Barber on 11/09/2017.
+//  Copyright © 2017 IBand. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
